c241fb88b1b174486128e812142e4d4db3611b31
BenCmjn <benjamin.calmejane@gmail.com>
2018-02-01 20:40:58 +0100

    derniere modif

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,10 @@
 class Course < ApplicationRecord
 	has_many :lessons, inverse_of: :course
-	
-	validates :title, :description, presence: true
+
+	validates :title, uniqueness: { case_sensitive: false }
+					length: { minimum: 2 }
+
+	validates :description, presence: true
 	
 end
+
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,6 +1,6 @@
 class Lesson < ApplicationRecord
 	belongs_to :course	
   	
-	validates :title, :body, :courses, presence: true
-	
+	validates :title, :body, presence: true
+	validates :courses, presence: true
 end
